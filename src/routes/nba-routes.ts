8a1a78f85eeff2dd24b378ba55b4d2ee5c6a5788
Rogerio <rogerlopes1999@hotmail.com>
2025-02-06 21:33:29 -0300

    FIltro pra posição de jogador vazia e mais dados dos jogadores

--- src/routes/nba-routes.ts
+++ src/routes/nba-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
-import { getPlayers, getTeams } from '@services/nba-service';
 import { BalldontlieAPI } from '@balldontlie/sdk';
 import AxiosClient from '@configs/axios/axios.config';
+import { Player } from '@interfaces/interfaces';
 
 const axios = AxiosClient.getInstance();
 
@@ -10,11 +10,14 @@ const api = new BalldontlieAPI({ apiKey: "f725831e-8411-452d-8213-b65bf2ec4080" 
 
 nbaRoutes.get('/players', async (req, res) => {
   const { team_ids } = req.query;
-  console.log("ESSA PORRA É O ID:" , team_ids);
-
   try {
     const players = await axios.get(`/players`, {params: {"team_ids[]": team_ids }});
-    res.json(players.data);
+    res.json({
+      data: players.data.data.filter((player: Player) => 
+        player.position && player.position.trim() !== ''
+      ),
+      meta: players.data.meta
+    });
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch players' });
   }
