0f30869e827efc8d710e25a94c0332b804db3fda
pedro99009 <pedrofc99009@gmail.com>
2025-03-19 14:55:31 -0300

    Backend pagina jogador

--- src/routes/nba-routes.ts
+++ src/routes/nba-routes.ts
@@ -9,14 +9,14 @@ const nbaRoutes = Router();
 const api = new BalldontlieAPI({ apiKey: "f725831e-8411-452d-8213-b65bf2ec4080" });
 
 nbaRoutes.get('/players', async (req, res) => {
-  const { team_ids } = req.query;
+  const { team_ids, player_ids } = req.query;
   try {
-    const players = await axios.get(`/players`, {params: {"team_ids[]": team_ids }});
+    const players = await axios.get(`/players`, {params: {"team_ids[]": team_ids, "player_ids[]": player_ids }});
     res.json({
-      data: players.data.data//.filter((player: Player) => 
-       // player.position && player.position.trim() !== ''
-      //),
-     // meta: players.data.meta
+      data: players.data.data.filter((player: Player) => 
+        player.position && player.position.trim() !== ''
+      ),
+      meta: players.data.meta
     });
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch players' });
