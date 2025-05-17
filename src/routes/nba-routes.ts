b41cda95a1cd07e02620a4b5b64ac94eee7558ec
pedro99009 <pedrofc99009@gmail.com>
2025-05-17 18:23:47 -0300

    wip: Página times com jogadores

--- src/routes/nba-routes.ts
+++ src/routes/nba-routes.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import { BalldontlieAPI } from '@balldontlie/sdk';
 import AxiosClient from '@configs/axios/axios.config';
 import { Player } from '@interfaces/interfaces';
 
@@ -23,20 +22,33 @@ nbaRoutes.get('/players', async (req, res) => {
 });
 
 nbaRoutes.get('/classification', async (req, res) => {
-  const { division } = req.query;
+  const { conference, season } = req.query; // lê os params query
+
+  // Padroniza valor para NBA API (East ou West)
+  const confParam = conference === 'West' ? 'West' : 'East';
+
   try {
-    const { data } = await axios.get(`https://stats.nba.com/stats/leaguedashptstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&Height=&LastNGames=0&LeagueID=&Location=&Month=0&OpponentTeamID=0&Outcome=&PORound=&PerMode=Totals&PlayerExperience=&PlayerOrTeam=Team&PlayerPosition=&PtMeasureType=SpeedDistance&Season=2024-25&SeasonSegment=&SeasonType=Regular+Season&StarterBench=&TeamID=&VsConference=&VsDivision=&Weight=`);
+    const { data } = await axios.get(
+      `https://stats.nba.com/stats/leaguedashptstats?College=&Conference=${confParam}` +
+      `&Country=&DateFrom=&DateTo=&Division=` +
+      `&DraftPick=&DraftYear=&GameScope=&Height=&LastNGames=0&LeagueID=&Location=` +
+      `&Month=0&OpponentTeamID=0&Outcome=&PORound=&PerMode=Totals&PlayerExperience=` +
+      `&PlayerOrTeam=Team&PlayerPosition=&PtMeasureType=SpeedDistance&Season=${season}` +
+      `&SeasonSegment=&SeasonType=Regular+Season&StarterBench=&TeamID=&VsConference=` +
+      `&VsDivision=&Weight=`
+    );
     res.json(data);
   } catch (error) {
-    console.error('Error fetching teams:', error);
-    res.status(500).json({ error: 'Failed to fetch teams' });
+    console.error('Error fetching classification:', error);
+    res.status(500).json({ error: 'Failed to fetch classification' });
   }
 });
+
 
 nbaRoutes.get('/teams', async (req, res) => {
-  const { division } = req.query;
+  const { division, teamId } = req.query;
   try {
-    const { data } = await axios.get(`https://stats.nba.com/stats/leaguedashptstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=${division}&DraftPick=&DraftYear=&GameScope=&Height=&LastNGames=0&LeagueID=&Location=&Month=0&OpponentTeamID=0&Outcome=&PORound=&PerMode=Totals&PlayerExperience=&PlayerOrTeam=Team&PlayerPosition=&PtMeasureType=SpeedDistance&Season=2024-25&SeasonSegment=&SeasonType=Regular+Season&StarterBench=&TeamID=&VsConference=&VsDivision=&Weight=`);
+    const { data } = await axios.get(`https://stats.nba.com/stats/leaguedashptstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=${division || ''}&DraftPick=&DraftYear=&GameScope=&Height=&LastNGames=0&LeagueID=&Location=&Month=0&OpponentTeamID=0&Outcome=&PORound=&PerMode=Totals&PlayerExperience=&PlayerOrTeam=Team&PlayerPosition=&PtMeasureType=SpeedDistance&Season=2024-25&SeasonSegment=&SeasonType=Regular+Season&StarterBench=&TeamID=${teamId || 0}&VsConference=&VsDivision=&Weight=`);
     res.json(data);
   } catch (error) {
     console.error('Error fetching teams:', error);
@@ -44,11 +56,23 @@ nbaRoutes.get('/teams', async (req, res) => {
   }
 });
 
-nbaRoutes.get('/allPlayers', async (req, res) => {
+nbaRoutes.get('/playerByTeam', async (req, res) => {
+  const { teamId } = req.query;
   try {
-    const teams = await axios.get(`/players`);
-    res.json(teams.data);
+    const { data } = await axios.get(`https://stats.nba.com/stats/playerindex?Active=&AllStar=&College=&Country=&DraftPick=&DraftRound=&DraftYear=&Height=&Historical=&LeagueID=00&Season=2022-23&TeamID=${teamId}&Weight=`);
+    res.json(data);
   } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch teams' });
+  }
+});
+
+nbaRoutes.get('/teamsById', async (req, res) => {
+  const { teamId } = req.query;
+  try {
+    const { data } = await axios.get(`https://stats.nba.com/stats/teaminfocommon?LeagueID=00&Season=&SeasonType=&TeamID=${teamId}`);
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching teams:', error);
     res.status(500).json({ error: 'Failed to fetch teams' });
   }
 });
